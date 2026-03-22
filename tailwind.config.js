239b44c155c7124d8c1860033edda947fd8debd7
PrasenjeetSC <171351044+PrasenjeetSC@users.noreply.github.com>
2026-03-22 21:11:25 +0530

    Tailwind moved from CDN to minified external css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.html", "./assets/js/**/*.js"],
+  content: ["./**/*.html"],
   theme: {
     extend: {
       colors: {
         brand: '#7c3aed',
         soft: '#fdf4ff'
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif']
       }
     }
   },
